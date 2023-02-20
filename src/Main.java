0eafe3bab478adfb32878ddad0a351036810b057
AHmedburitos <ahmed.buric@2gimnazija.edu.ba>
2023-02-20 16:55:16 +0100

    zadatak11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Selammmmm!");
+
+        int broj;
+        int suma =0;
+        Scanner ulaz = new Scanner(System.in); // Ulazni objekat
+        System.out.println("unes te broj: ");
+        broj = ulaz.nextInt();
+        for (int i = 1; i <broj+1; i++) {
+            suma=i%10+((i-i%10)/10);
+
+            if(i%suma==0)
+            {
+                System.out.println("broj je paran:" + i);
+            }
+        }
+
     }
 }
